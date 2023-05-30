dd64b5c3f5d2e835deadb0dabfec2025a38dd111
leontomer <41841316+leontomer@users.noreply.github.com>
2023-05-30 15:51:53 +0300

    fixed some bug on updating courses

--- client/src/components/courseprogress/CourseProgress.js
+++ client/src/components/courseprogress/CourseProgress.js
@@ -6,11 +6,17 @@ import "./CourseProgress.css";
 export default function CourseProgress() {
   const { selectedCourse, setSelectedCourse, setCourses, courses } =
     useContext(CoursesContext);
+  console.log(selectedCourse);
 
   const handleChangeLessonStatus = (index) => {
     selectedCourse.lessons[index] = !selectedCourse.lessons[index];
     setSelectedCourse(selectedCourse);
-    setCourses([...courses, selectedCourse]);
+    for (let i = 0; i < courses.length; i++) {
+      if (courses[i] === selectedCourse.courseId) {
+        courses[i] = selectedCourse;
+      }
+    }
+    setCourses([...courses]);
   };
   return (
     <div className="course-completion-container">
